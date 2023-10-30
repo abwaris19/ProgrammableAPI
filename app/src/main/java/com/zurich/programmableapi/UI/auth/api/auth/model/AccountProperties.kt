1bd1c51bad400705e8c431668bb50c17fdd20d29
abwaris1996 <hellokhan.ch@gmail.com>
2023-10-30 21:48:05 +0400

    define table name and columns

--- app/src/main/java/com/zurich/programmableapi/UI/auth/api/auth/model/AccountProperties.kt
+++ app/src/main/java/com/zurich/programmableapi/UI/auth/api/auth/model/AccountProperties.kt
@@ -1,2 +1,34 @@
 package com.zurich.programmableapi.UI.auth.api.auth.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+@Entity (tableName = "account_properties")
+data class AccountProperties(
+
+
+    @SerializedName("pk")
+    @Expose
+    @PrimaryKey (autoGenerate = false)
+    @ColumnInfo(name = "pk")
+    var pk: Int,
+
+    @SerializedName("email")
+    @Expose
+    @PrimaryKey (autoGenerate = false)
+    @ColumnInfo(name = "email")
+    var email: String,
+
+    @SerializedName("username")
+    @Expose
+    @PrimaryKey (autoGenerate = false)
+    @ColumnInfo(name = "username")
+    var username: String,
+
+
+
+)
