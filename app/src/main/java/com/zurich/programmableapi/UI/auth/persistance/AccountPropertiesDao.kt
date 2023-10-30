629f05b0a445075803425e3e161390838b62a3a2
abwaris1996 <hellokhan.ch@gmail.com>
2023-10-30 22:09:17 +0400

    Defined the AccountPropertiesDao Insert and select Queries

--- app/src/main/java/com/zurich/programmableapi/UI/auth/persistance/AccountPropertiesDao.kt
+++ app/src/main/java/com/zurich/programmableapi/UI/auth/persistance/AccountPropertiesDao.kt
@@ -1,5 +1,27 @@
 package com.zurich.programmableapi.UI.auth.persistance
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.zurich.programmableapi.UI.auth.api.auth.model.AccountProperties
+
+@Dao
 interface AccountPropertiesDao{
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAndReplace(accountProperties: AccountProperties): Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun  insertOrIgnore(accountProperties: AccountProperties): Long
+
+    @Query(value = "SELECT *FROM account_properties WHERE pk = :pk")
+    fun searchByPk(pk: Int): AccountProperties?
+
+    @Query(value = "SELECT *FROM account_properties  WHERE email = :email")
+    fun searchByEmail (email: String): AccountProperties?
+
+
 
 }
