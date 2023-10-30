93098376f27b567a05f34162218f9fd1ff819742
abwaris1996 <hellokhan.ch@gmail.com>
2023-10-30 22:17:31 +0400

    defined entities and database name

--- app/src/main/java/com/zurich/programmableapi/UI/auth/persistance/AppDatabase.kt
+++ app/src/main/java/com/zurich/programmableapi/UI/auth/persistance/AppDatabase.kt
@@ -1,6 +1,20 @@
 package com.zurich.programmableapi.UI.auth.persistance
 
+import androidx.room.Database
+import com.zurich.programmableapi.UI.auth.api.auth.model.AccountProperties
+import com.zurich.programmableapi.UI.auth.api.auth.model.AuthToken
 
-annotation class AppDatabase {
 
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase {
+
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+
+    companion object {
+        const val DATABASE_NAME ="app_db"
+    }
 }
