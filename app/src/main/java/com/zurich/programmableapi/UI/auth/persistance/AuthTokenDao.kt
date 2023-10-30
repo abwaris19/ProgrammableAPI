afe678bcc0196b35b7fa11afc80822caa2e063e7
abwaris1996 <hellokhan.ch@gmail.com>
2023-10-30 22:12:03 +0400

    Defined the AuthTokenDao nullifyToken and insert Queries

--- app/src/main/java/com/zurich/programmableapi/UI/auth/persistance/AuthTokenDao.kt
+++ app/src/main/java/com/zurich/programmableapi/UI/auth/persistance/AuthTokenDao.kt
@@ -1,5 +1,20 @@
 package com.zurich.programmableapi.UI.auth.persistance
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.zurich.programmableapi.UI.auth.api.auth.model.AuthToken
+
+@Dao
 interface AuthTokenDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert (authToken: AuthToken): Long
+
+    @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
+    fun nullifyToken(pk: Int): Int
+
 
 }
